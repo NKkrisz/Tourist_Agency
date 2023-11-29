afaf75d4bb3b03e19f2773e1482222a51d11b392
NKkrisz <nkkrisz2005@gmail.com>
2023-11-29 08:41:00 +0100

    Dialog and a bit of styling

--- index.js
+++ index.js
@@ -5,11 +5,29 @@ async function showPlaces() {
 	.then(data => {
         data.forEach(element => {
             let place = document.createElement("div");
-            place.classList.add("flex", "flex-col", "max-w-60", "max-h-60", "p-4", "bg-slate-700", "border", "border-2", "rounded-lg");
-            place.appendChild(document.createElement("h2")).innerText = element.name;
-            place.appendChild(document.createElement("img")).src = element.image;
-            place.appendChild(document.createElement("p")).innerText = `Price: ${element.price}$`;
+            place.classList.add("flex", "flex-col", "items-center", "max-w-1/4", "max-h-1/4", "p-4", "bg-slate-700", "border", "border-2", "rounded-lg");
+            
+            const placeName = document.createElement("h2")
+            placeName.innerText = element.name;
+            const placeImg = document.createElement("img")
+            placeImg.src = element.image;
+            placeImg.classList.add("w-32", "h-32");
+            const placeText = document.createElement("p")
+            placeText.innerText = `Price: ${element.price}$`;
+            
+            place.appendChild(placeName);
+            place.appendChild(placeImg);
+            place.appendChild(placeText);
+            
             place.onclick = () => {
+                infoDialog.innerHTML = "";
+                const closeButton = document.createElement("button");
+                closeButton.classList.add("w-20")
+                closeButton.innerText = "Close";
+                closeButton.onclick = () => {
+                    infoDialog.close();
+                }
+                infoDialog.appendChild(closeButton);
                 infoDialog.showModal();
                 infoDialog.appendChild(document.createElement("h3")).innerText = element.name;
                 infoDialog.appendChild(document.createElement("p")).innerText = element.info;
